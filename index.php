49b5defb87de760b41877ccc2060ee313b15bf1a
Matt David <mcdav3636@gmail.com>
2017-11-04 19:48:26 -0600

    added too much shit!!!

--- index.php
+++ index.php
@@ -6,6 +6,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+    <!-- CSS style link -->
+    <link rel="stylesheet" href="css/styles.css" type="text/css" />
+
+
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
@@ -29,7 +33,7 @@
 <section class="jumbotron-style">
 <div class="container">
     <div class="row">
-        <div class="col-lg-12 col-xs-12" >
+        <div class="col-sm-12" >
             <div class="jumbotron">
 <h1>Hello, world!</h1>
 <p>This is a simple hero unit, a simple jumbotron-stle component for calling extra attention to featured content or
@@ -40,6 +44,50 @@ information.</p>
     </div>
 </div>
 </section>
+
+<!-- section for image content -->
+
+    <div class="container-fluid">
+        <div class="row">
+         <div class="col-sm-4">
+            <img src="images/ktm.jpg" class="img-fluid">
+         </div>
+         <div class="col-sm-8">
+             <p>This was my last girlfriend. she was a great girlfriend.</p>
+         </div>
+        </div>
+    </div>
+
+<!-- class for later loves of my life section -->
+<section class="later-ladies">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-3">
+                <img src="images/the-harley.jpg" alt="my first harley" class="img-fluid">
+                <p>this is my first harley. she was great. a little too small but great. my wife didn't really love
+                going with me so I decided I didn't want it as much as a dirt bike</p>
+            </div>
+            <div class="col-sm-3">
+                <img src="images/kx250f.jpg" alt="kawasaki dirt bike" class="img-fluid">
+                <p>This little sweetheart really rocked my world for a while. She was fast and mean. She pushed me
+                as much as I pushed her</p>
+            </div>
+            <div class="col-sm-3">
+                <img src="images/honda-in-the-back.jpg" alt="hidden honda dirt bike" class="img-fluid">
+                <p>I must not have cared about this old gal too much cause this is the best picture i got and she is 
+                hidden behind the kawasaki</p>
+            </div>
+            <div class="col-sm-3">
+                <img src="images/first-honda.jpg" alt="old honda dirt bike" class="img-fluid">
+                <p>This tiny princess didn't fair so well. i thought i was buying a dirt bike that was up to the
+                task but it turns out that her suspension was not meant for what i wanted her for.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- footer -->
+
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
